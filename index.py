9c9e596e3914242f78c6a37b55c39a83f400eca1
seanp97 <sean.pelser97@gmail.com>
2020-02-06 14:34:59 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
 import random
+from tkinter import *
+import sys
+import tkinter
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"  "k", "l", "m", "n",
 "o", "p", "q",  "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -6,12 +9,36 @@ alphabetLen = len(alphabet)
 
 myNewPass = []
 finishedPass = ""
-passLen = input("How long do you want your password to be? \n")
 
-for i in range(int(passLen)):
-    randPass = random.randrange(alphabetLen)
-    myNewPass = alphabet[randPass]
-    finishedPass += str(myNewPass)
+def generatePassword():
+    passwordLength = Entry.get(passLenEnt)
+    for i in range(int(passwordLength)):
+        global finishedPass
+        randPass = random.randrange(alphabetLen)
+        myNewPass = alphabet[randPass]
+        finishedPass += str(myNewPass)
 
-newPass = finishedPass
-print("Your generated password is",newPass)
+    newPass = finishedPass
+    passEmptyLabel.configure(text=newPass)
+    myNewPass = []
+    finishedPass = ""
+
+app = Tk()
+app.title("Calculator")
+app.geometry("900x450")
+
+
+passLabel = Label(app, text="Random Password Generator A-Z", padx=10, pady=10, font='Helvetica 14 bold')
+passLabel.grid(column=0, row=0)
+
+passLenLabel = Label(app, text="How long do you want your password to be?", padx=10, pady=10, font='Helvetica 8 bold')
+passLenLabel.grid(column=0, row=1)
+passLenEnt = Entry(app, bd=5)
+passLenEnt.grid(column=0, row=2)
+passLenBtn = Button(app, bd=5, text="Sumbit", padx=5, pady=5, command=generatePassword)
+passLenBtn.grid(column=0, row=3)
+
+passEmptyLabel = Label(app, text="", padx=10, pady=15, font='Helvetica 8 bold')
+passEmptyLabel.grid(column=0, row=4)
+
+app.mainloop()
